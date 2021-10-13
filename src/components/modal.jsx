860ac962d0f1f8296c91dac5d0f961b412220a4b
Mikhail Lapitskiy <hiimisha@mail.ru>
2021-10-14 00:34:26 +0300

    hide the field if there is no data

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -26,7 +26,7 @@ const ModalCharacter = (props) => {
                         <p>Gender: {props.gender}</p>
                         <p>Status: {props.status}</p>
                         <p>Species: {props.species}</p>
-                        <p>Type: {props.type}</p>
+                        <p className={(props.type === "" ? "d-none" : "")}>Type: {props.type}</p>
                         <p>Last known location: {props.location.name}</p>
                     </div>
                 </div>
